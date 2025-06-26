486994ee9107ba638142395a57a03aa7a2e6591f
Mirko Mariotti <mirko@mirkomariotti.it>
2025-06-26 12:08:09 +0200

    03

--- bmexamples/03_toolchain_bis_7seg/counter.go
+++ bmexamples/03_toolchain_bis_7seg/counter.go
@@ -5,18 +5,22 @@ import (
 )
 
 func main() {
-	var outgoing bondgo.Output
+	var outgoing1 bondgo.Output
+	var outgoing2 bondgo.Output
 
-	var reg_int uint8
+	var reg_int uint16
 
-	outgoing = bondgo.Make(bondgo.Output, 3)
+	outgoing1 = bondgo.Make(bondgo.Output, 3)
+	outgoing2 = bondgo.Make(bondgo.Output, 4)
 
 	reg_int = 0
 
 	for {
-		bondgo.IOWrite(outgoing, reg_int)
+		bondgo.IOWrite(outgoing1, reg_int)
+		bondgo.IOWrite(outgoing2, reg_int)
 		reg_int++
 	}
 
-	bondgo.Void(outgoing)
+	bondgo.Void(outgoing1)
+	bondgo.Void(outgoing2)
 }
